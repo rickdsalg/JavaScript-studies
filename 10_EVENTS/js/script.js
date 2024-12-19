7382d59449fe0a60962acee05ebd31ed8ced291b
Ricardo Salgado <ricardodsalg@gmail.com>
2024-12-19 14:58:52 -0500

    lecture #3 - event argument

--- 10_EVENTS/js/script.js
+++ 10_EVENTS/js/script.js
@@ -22,3 +22,13 @@ thirdBtn.addEventListener("click", ()=> {
     secondBtn.removeEventListener("click", clickMessage);
 
 })
+
+// 3 - event argument
+const myTitle = document.querySelector('#my-title')
+
+myTitle.addEventListener("click", (event) => {
+    console.log(event);
+    console.log(event.offsetX);
+    console.log(event.pointerType);
+    console.log(event.target);
+})
