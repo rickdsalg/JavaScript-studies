9302016c32119e206b92e1a204b72814b780a234
Ricardo Salgado <ricardodsalg@gmail.com>
2024-12-19 14:39:25 -0500

    Class 2 - event removal
    Learning how to remove events

--- 10_EVENTS/js/script.js
+++ 10_EVENTS/js/script.js
@@ -3,4 +3,22 @@ const btn = document.querySelector('#my-button')
 
 btn.addEventListener("click", function(){
     console.log("Button clicked")
+});
+
+// 2 - Removing event
+const secondBtn = document.querySelector("#btn");
+
+function clickMessage(){
+    console.log("Event added")
+}
+
+secondBtn.addEventListener("click", clickMessage);
+
+const thirdBtn = document.querySelector("#other-btn");
+
+thirdBtn.addEventListener("click", ()=> {
+
+    console.log('Event removed');
+    secondBtn.removeEventListener("click", clickMessage);
+
 })
